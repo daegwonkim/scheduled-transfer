e5b70a311a3590e79ce48298f8370b39ee15e093
kimdaegwon <daegwonkim.dev@gmail.com>
2025-09-29 14:27:03 +0900

    프로젝트 생성

---
+++ src/main/java/io/github/daegwon/delayed_transfer/DelayedTransferApplication.java
@@ -0,0 +1,13 @@
+package io.github.daegwon.delayed_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayedTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelayedTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/daegwon/delayed_transfer/DelayedTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.daegwon.delayed_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelayedTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
